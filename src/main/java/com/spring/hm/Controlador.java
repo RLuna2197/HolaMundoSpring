147442ea8978c3bf449e8524f665c8635051cbf3
roberto.luna <roberto.luna@salud.gob.sv>
2023-01-05 08:25:46 -0600

    lista de personas agregada

--- src/main/java/com/spring/hm/Controlador.java
+++ src/main/java/com/spring/hm/Controlador.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 @Slf4j
 public class Controlador {
@@ -21,10 +23,18 @@ public class Controlador {
         persona.setEmail("preuba@gmail.com");
         persona.setTelefono("2252522");
 
+        var persona2 = new Persona();
+        persona2.setNombre("Roberto");
+        persona2.setApellido("perez");
+        persona2.setEmail("preuba2@gmail.com");
+        persona2.setTelefono("7252522");
+
+        var personas = Arrays.asList(persona, persona2);
+
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
 
         return  "index";
     }
